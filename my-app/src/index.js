879a533b9e59a6dc0b21e4627094c66ae389146f
ajujawade <ajayjawade06@gmail.com>
2024-08-06 15:37:23 +0530

    added

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import './index.css';
-import App from './App';
-import App2 from './App2';
-import Functionone from './Functionone';
-import FunctionTwo from './FunctionTwo';
-import FunctionThree from './Function3';
-import FunctionFour from './Function4';
-import FunctionFive from './Function5';
-import FunctionSix from './Function6';
-import FunctionSeven from './Function7';
-import FunctionEight from './Function8';
-import ClassBasics from './ClassBasics';
-import ClassProps from './ClassProps';
+// import App from './App';
+// import App2 from './App2';
+// import Functionone from './Functionone';
+// import FunctionTwo from './FunctionTwo';
+// import FunctionThree from './Function3';
+// import FunctionFour from './Function4';
+// import FunctionFive from './Function5';
+// import FunctionSix from './Function6';
+// import FunctionSeven from './Function7';
+// import FunctionEight from './Function8';
+// import ClassBasics from './ClassBasics';
+// import ClassProps from './ClassProps';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -29,7 +29,7 @@ root.render(
     {/* <FunctionSeven></FunctionSeven> */}
     {/* <FunctionEight></FunctionEight> */}
     {/* <ClassBasics></ClassBasics> */}
-    <ClassProps greet = "Ajay" greet2 = "Tejas"></ClassProps>
+    {/* <ClassProps greet = "Ajay" greet2 = "Tejas"></ClassProps> */}
   </React.StrictMode>
 );
 
